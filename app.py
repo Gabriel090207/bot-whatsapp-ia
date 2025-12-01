0cb27c921d32346f3d1ff86a9f7bab1606b7fe67
Gabriel090207 <gabrielborela07@gmail.com>
2025-12-01 07:48:10 -0300

    port

--- app.py
+++ app.py
@@ -115,4 +115,6 @@ def receber_mensagem():
     return "OK", 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
